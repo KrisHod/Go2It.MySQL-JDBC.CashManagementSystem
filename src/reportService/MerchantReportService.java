1cf110003b24d60ea245005a768b84773a5650b4
KrisHod <krisgodom@gmail.com>
2021-02-10 23:38:04 -0400

    refactored MerchantReportService

--- src/reportService/MerchantReportService.java
+++ src/reportService/MerchantReportService.java
@@ -1,45 +1,34 @@
-package ReportService;
+package reportService;
 
-import entities.Merchant;
-import entities.Payment;
+import entity.Merchant;
+import entity.Payment;
 import service.MerchantService;
 import service.PaymentService;
+import util.MerchantRepository;
+import util.PaymentRepository;
 
 import java.util.List;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class MerchantReportService {
-    // Create a specific class and method that will show a total sum paid for a merchant with a given id (argument).
-    // The report should also contain merchant_id, title and lastSent info
+    PaymentRepository paymentRepository = new PaymentRepository();
+    MerchantRepository merchantRepository = new MerchantRepository();
 
-    PaymentService paymentService = new PaymentService();
-    MerchantService merchantService = new MerchantService();
-
-    public double calculateTotalSum(int id) {
-        List<Payment> payments = paymentService.createPaymentList();
-        double totalSum = 0;
-
-        for (Payment payment : payments) {
-            if (payment.getMerchant().getId() == id) {
-                totalSum = +payment.getSumPaid();
-            }
-        }
-        return totalSum;
-    }
-
-    public void showReport(int id) {
-        Merchant merchant = merchantService.getById(id);
-        System.out.println("Total sum paid: " + calculateTotalSum(id) + ". Merchant Id: " + id + "," +
+    //The report shows a total sum paid for a merchant with a given id (argument), merchant_id, title and lastSent info
+    public void showReportById(int id) {
+        Merchant merchant = merchantRepository.getById(id);
+        System.out.println("Total sum paid: " + paymentRepository.getTotalSumPaid(id) + ". Merchant Id: " + id + "," +
                 " title: " + merchant.getName() + ", last sent: " + merchant.getLastSent());
     }
 
     //Create an application to display a list of all merchants sorted alphabetically in descending order
     public void showSortedDescendingOrder() {
-        TreeSet<String> names = new TreeSet<>();
-        List<Merchant> merchants = merchantService.createMerchantList();
+        Set<String> names = new TreeSet<>();
+        List<Merchant> merchants = merchantRepository.getAll();
         for (Merchant mer : merchants) {
             names.add(mer.getName());
         }
-        System.out.println((TreeSet<String>) names.descendingSet());
+        System.out.println(((TreeSet<String>) names).descendingSet());
     }
 }
