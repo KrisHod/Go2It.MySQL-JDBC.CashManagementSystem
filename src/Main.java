cf368d92ff81abd20e615d0db6cb3a4fd109462d
Kris <krisgodom@gmail.com>
2021-02-15 18:03:17 -0400

    added Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Main {
+import entity.Payment;
+import reportService.MerchantReportService;
+import service.CustomerService;
+import service.MerchantService;
+import service.PaymentService;
+import util.MerchantRepository;
+import util.PaymentRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        MerchantService merchantService = new MerchantService();
+        CustomerService customerService = new CustomerService();
+        PaymentService paymentService = new PaymentService();
+
+        MerchantReportService merchantReportService = new MerchantReportService();
+
+        // clause 2
+        merchantReportService.showReportById(1);
+
+        // clause 3
+        merchantReportService.showSortedDescendingOrder();
+
+        //clause 4 & 5
+        Payment payment = new Payment(LocalDateTime.of(2012, 07, 01, 6, 23, 00),
+                merchantService.getById(3), customerService.getById(1), "book", 35);
+        paymentService.addPayment(payment);
+
+        //clause 7
+        System.out.println(customerService.findMostActiveCustomer(LocalDate.of(2012, 07, 01), LocalDate.of(2012, 07, 30)));
+
+    }
 }
--- src/reportService/MerchantReportService.java
+++ src/reportService/MerchantReportService.java
@@ -4,6 +4,7 @@ import entity.Merchant;
 import entity.Payment;
 import service.MerchantService;
 import service.PaymentService;
+import util.CustomerRepository;
 import util.MerchantRepository;
 import util.PaymentRepository;
 
@@ -11,21 +12,22 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class MerchantReportService {
+public class ReportService {
+    MerchantService merchantService = new MerchantService();
     PaymentRepository paymentRepository = new PaymentRepository();
-    MerchantRepository merchantRepository = new MerchantRepository();
+
 
     //The report shows a total sum paid for a merchant with a given id (argument), merchant_id, title and lastSent info
     public void showReportById(int id) {
-        Merchant merchant = merchantRepository.getById(id);
-        System.out.println("Total sum paid: " + paymentRepository.getTotalSumPaid(id) + ". Merchant Id: " + id + "," +
+        Merchant merchant = merchantService.getById(id);
+        System.out.println("Total sum paid: " + paymentRepository.getTotalSumPaid(merchant) + ". Merchant Id: " + id + "," +
                 " title: " + merchant.getName() + ", last sent: " + merchant.getLastSent());
     }
 
     //Create an application to display a list of all merchants sorted alphabetically in descending order
     public void showSortedDescendingOrder() {
         Set<String> names = new TreeSet<>();
-        List<Merchant> merchants = merchantRepository.getAll();
+        List<Merchant> merchants = merchantService.getAll();
         for (Merchant mer : merchants) {
             names.add(mer.getName());
         }
