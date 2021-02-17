bfbd76c0e5cba2ea21a97fcf97327d81824c61c7
KrisHod <krisgodom@gmail.com>
2021-02-16 22:42:46 -0400

    minor changes

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -8,12 +8,33 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class CustomerService {
-    private CustomerRepository customerRepository = new CustomerRepository();
-    private PaymentService paymentService = new PaymentService();
+    private CustomerRepository customerRepository;
+    private PaymentService paymentService;
+
+    public CustomerService() {
+    }
+
+    public CustomerRepository getCustomerRepository() {
+        return customerRepository;
+    }
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public PaymentService getPaymentService() {
+        return paymentService;
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public List<Customer> getAll (){
         return customerRepository.getAll();
     }
+
+    public Customer getById(int id, boolean isPaymentKnown) { return customerRepository.getById(id, isPaymentKnown);}
 
     //  Find the most active customer based on the number of order within the passed in time period (ie week, month, quarter, year).
     //  The resulting Customer object should contain the list of all Payments made.
@@ -22,7 +43,6 @@ public class CustomerService {
         Customer theMostActiveCustomer = null;
         List<Payment> paymentsByPeriod = paymentService.getByPeriod(startDate, endDate);
         List<Customer> customers = customerRepository.getAll();
-        System.out.println(customers);
         for (Payment p : paymentsByPeriod) {
             for (Customer c : customers) {
                 if (p.getCustomer().equals(c)) {
@@ -39,7 +59,6 @@ public class CustomerService {
         }
        return theMostActiveCustomer;
     }
-
 }
 
 
