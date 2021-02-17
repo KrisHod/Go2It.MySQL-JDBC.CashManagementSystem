3ea4ea85e75ad6811a0508fe7f482890257d8dee
KrisHod <krisgodom@gmail.com>
2021-02-16 22:43:22 -0400

    minor changes

--- src/service/MerchantService.java
+++ src/service/MerchantService.java
@@ -1,13 +1,44 @@
 package service;
 
+import entity.Merchant;
 import entity.Payment;
 import util.MerchantRepository;
 
-public class MerchantService {
-    MerchantRepository merchantRepository = new MerchantRepository();
+import java.util.List;
 
-    public boolean getAll() {
-        return merchantRepository.getAll().isEmpty();
+public class MerchantService {
+    PaymentService paymentService;
+    MerchantRepository merchantRepository;
+
+    public MerchantService() {
+    }
+
+    public PaymentService getPaymentService() {
+        return paymentService;
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public MerchantRepository getMerchantRepository() {
+        return merchantRepository;
+    }
+
+    public void setMerchantRepository(MerchantRepository merchantRepository) {
+        this.merchantRepository = merchantRepository;
+    }
+
+    public List<Merchant> getAll() {
+        return merchantRepository.getAll();
+    }
+
+    public Merchant getById(int id, boolean isPaymentKnown) {
+        return merchantRepository.getById(id, isPaymentKnown);
+    }
+
+    public double calculateNeedToSend(Payment payment) {
+        return payment.getMerchant().getNeedToSend() + (payment.getSumPaid() - payment.getMerchant().getCharge());
     }
 
 }
