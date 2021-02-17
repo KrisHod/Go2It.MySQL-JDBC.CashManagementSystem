f047d118faa3cb31b2d3533c6f57a71dfe3feab5
KrisHod <krisgodom@gmail.com>
2021-02-17 00:11:11 -0400

    added getMerchant

--- src/util/MerchantRepository.java
+++ src/util/MerchantRepository.java
@@ -1,6 +1,5 @@
 package util;
 
-import entity.Customer;
 import entity.Merchant;
 import entity.Payment;
 
@@ -10,29 +9,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MerchantRepository {
-    public Merchant getById(int id) {
+    PaymentRepository paymentRepository;
+
+    public PaymentRepository getPaymentRepository() {
+        return paymentRepository;
+    }
+
+    public void setPaymentRepository(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    public MerchantRepository() {
+    }
+
+    public MerchantRepository(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    public Merchant getMerchant(ResultSet rs, boolean isPaymentKnown) throws SQLException {
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        String bankName = rs.getString("bankName");
+        String swift = rs.getString("swift");
+        String account = rs.getString("account");
+        double charge = rs.getDouble("charge");
+        int period = rs.getInt("period");
+        double minSum = rs.getDouble("minSum");
+        double needToSend = rs.getDouble("needToSend");
+        double sent = rs.getDouble("sent");
+        LocalDate lastSent = rs.getDate("lastSent") == null ? null : rs.getDate("lastSent").toLocalDate();
+        Merchant merchant = new Merchant(id, name, bankName, swift, account, charge, period, minSum, needToSend, sent, lastSent);
+
+        if (!isPaymentKnown) {
+            List<Payment> payments = paymentRepository.getByMerchant(merchant);
+            merchant.setPayments(payments);
+        }
+        return merchant;
+    }
+
+    public Merchant getById(int id, boolean isPaymentKnown) {
         Merchant merchant = null;
         try (Connection con = DBUtil.getConnection();
              PreparedStatement stmt = con.prepareStatement("SELECT * FROM merchant WHERE id=" + id)) {
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
-                String name = rs.getString("name");
-                String bankName = rs.getString("bankName");
-                String swift = rs.getString("swift");
-                String account = rs.getString("account");
-                double charge = rs.getDouble("charge");
-                int period = rs.getInt("period");
-                double minSum = rs.getDouble("minSum");
-                double needToSend = rs.getDouble("needToSend");
-                double sent = rs.getDouble("sent");
-                LocalDate lastSent = rs.getDate("lastSent") == null ? null : rs.getDate("lastSent").toLocalDate();
-                merchant = new Merchant(id, name, bankName, swift, account, charge, period, minSum, needToSend, sent, lastSent);
+                merchant = getMerchant(rs, isPaymentKnown);
             }
-            return merchant;
         } catch (SQLException ex) {
             System.out.println("Error " + ex.getMessage());
         }
-        return null;
+        return merchant;
     }
 
 
@@ -44,28 +70,26 @@ public class MerchantRepository {
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
                 int id = rs.getInt("id");
-                merchants.add(getById(id));
+                merchants.add(getMerchant(rs, false));
             }
-            return merchants;
         } catch (SQLException ex) {
             System.out.println("Error " + ex.getMessage());
         }
-        return null;
+        return merchants;
     }
 
     public void update(Payment payment) {
-        double needToSend = payment.getMerchant().getNeedToSend() + (payment.getSumPaid() - payment.getMerchant().getCharge()); //pick out to another method???
         PreparedStatement stmt = null;
         try (Connection con = DBUtil.getConnection()) {
-            if (needToSend > payment.getMerchant().getMinSum()) {
+            if (payment.getMerchant().getNeedToSend() > payment.getMerchant().getMinSum()) {
                 stmt = con.prepareStatement("UPDATE merchant SET needToSend = ?, sent = ?, lastSent = ? WHERE id =" + payment.getMerchant().getId());
                 stmt.setDouble(1, 0);                                           //reset value of needToSend
-                payment.getMerchant().setSent(payment.getMerchant().getSent() + needToSend);   //add this sum(needToSend) to existing amount(sent)
+                payment.getMerchant().setSent(payment.getMerchant().getSent() + payment.getMerchant().getNeedToSend());   //add this sum(needToSend) to existing amount(sent)
                 stmt.setDouble(2, payment.getMerchant().getSent());
                 stmt.setDate(3, java.sql.Date.valueOf(payment.getDt().toLocalDate())); //update the date of the last transaction
             } else {
                 stmt = con.prepareStatement("UPDATE merchant SET needToSend = ? WHERE id = " + payment.getMerchant().getId());
-                stmt.setDouble(1, needToSend);
+                stmt.setDouble(1, payment.getMerchant().getNeedToSend());
             }
         } catch (SQLException ex) {
             System.out.println("Error " + ex.getMessage());
