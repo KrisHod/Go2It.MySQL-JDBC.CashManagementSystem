a475ae3504cbf0e8dadd5074b4b52f31dd746007
KrisHod <krisgodom@gmail.com>
2021-02-18 23:00:01 -0400

    minor changes

--- src/reportService/MerchantReportService.java
+++ src/reportService/MerchantReportService.java
@@ -1,20 +1,16 @@
 package reportService;
 
 import entity.Merchant;
-import entity.Payment;
 import service.MerchantService;
 import service.PaymentService;
-import util.CustomerRepository;
-import util.MerchantRepository;
-import util.PaymentRepository;
 
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class MerchantReportService {
-    MerchantService merchantService;
-    PaymentService paymentService;
+    private MerchantService merchantService;
+    private PaymentService paymentService;
 
     public MerchantReportService() {
     }
@@ -36,7 +32,7 @@ public class MerchantReportService {
     }
 
     //The report shows a total sum paid for a merchant with a given id (argument), merchant_id, title and lastSent info
-    public void showReportById(int id) {
+    public void showTotalSumPaid(int id) {
         Merchant merchant = merchantService.getById(id, true);
         System.out.println("Total sum paid: " + paymentService.getTotalSumPaid(merchant) + ". Merchant Id: " + id + "," +
                 " title: " + merchant.getName() + ", last sent: " + merchant.getLastSent());
